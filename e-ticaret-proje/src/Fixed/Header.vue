2eb965149be167cb3c799e0a36575547a51b4aa4
Ibrahim Yerli <yrlibrahim25@gmail.com>
2024-09-03 17:47:00 +0300

    header remake

--- e-ticaret-proje/src/Fixed/Header.vue
+++ e-ticaret-proje/src/Fixed/Header.vue
@@ -1,124 +1,163 @@
 <template>
-  <header>
-    <div class="header-left-side flex items-center gap-8">
-      <div class="header-logo">
-        <router-link to="/">e-Market</router-link>
-      </div>
-      <div class="header-categorys flex gap-5">
-        <!-- Electronics Start -->
-        <div class="menu">
-          <RouterLink to="/Electronics">Electronics</RouterLink>
-          <div class="dropdown-menu">
-            <ul>
-              <li>
-                <router-link to="/SmartPhones">Smart Phones</router-link>
-              </li>
-              <li>
-                <router-link to="/Laptops">Laptops</router-link>
-              </li>
-            </ul>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="relative flex h-16 items-center justify-between">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton
+            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+          >
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div
+          class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start"
+        >
+          <div class="flex flex-shrink-0 items-center">
+            <img
+              class="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+          </div>
+          <div class="hidden sm:ml-6 sm:block">
+            <div class="flex space-x-4">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                :class="[
+                  item.current
+                    ? 'bg-gray-900 text-white'
+                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'rounded-md px-3 py-2 text-sm font-medium',
+                ]"
+                :aria-current="item.current ? 'page' : undefined"
+                >{{ item.name }}</a
+              >
+            </div>
           </div>
         </div>
-        <!-- Electronics End -->
-        <!-- Cosmetics Start -->
-        <div class="menu">
-          <RouterLink to="/Cosmetics">Cosmetics</RouterLink>
-          <div class="dropdown-menu">
-            <ul>
-              <li>
-                <router-link to="/Fragrances">Fragrances</router-link>
-              </li>
-              <li>
-                <router-link to="/Beauty">Beauty</router-link>
-              </li>
-            </ul>
-          </div>
+        <div
+          class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+        >
+          <button
+            type="button"
+            class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+          >
+            <span class="absolute -inset-1.5" />
+            <span class="sr-only">View notifications</span>
+            <BellIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+
+          <!-- Profile dropdown -->
+          <Menu as="div" class="relative ml-3">
+            <div>
+              <MenuButton
+                class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              >
+                <span class="absolute -inset-1.5" />
+                <span class="sr-only">Open user menu</span>
+                <img
+                  class="h-8 w-8 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </MenuButton>
+            </div>
+            <transition
+              enter-active-class="transition ease-out duration-100"
+              enter-from-class="transform opacity-0 scale-95"
+              enter-to-class="transform opacity-100 scale-100"
+              leave-active-class="transition ease-in duration-75"
+              leave-from-class="transform opacity-100 scale-100"
+              leave-to-class="transform opacity-0 scale-95"
+            >
+              <MenuItems
+                class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              >
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Your Profile</a
+                  >
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Settings</a
+                  >
+                </MenuItem>
+                <MenuItem v-slot="{ active }">
+                  <a
+                    href="#"
+                    :class="[
+                      active ? 'bg-gray-100' : '',
+                      'block px-4 py-2 text-sm text-gray-700',
+                    ]"
+                    >Sign out</a
+                  >
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
         </div>
-        <!-- Cosmetics End -->
-        <RouterLink to="/Groceries">Groceries</RouterLink>
-        <RouterLink to="/Home-Decoration">Home Decoration</RouterLink>
       </div>
     </div>
-    <div class="header-right-side">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 576 512"
-        class="header-shopping-icon"
-      >
-        <path
-          d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
-        />
-      </svg>
-      <div class="header-log-in">
-        <div v-if="isLogged">
-          <router-link to="Login">Giriş Yap</router-link>
-        </div>
-        <div v-else>
-          <div>Çıkış Yap</div>
-        </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="space-y-1 px-2 pb-3 pt-2">
+        <DisclosureButton
+          v-for="item in navigation"
+          :key="item.name"
+          as="a"
+          :href="item.href"
+          :class="[
+            item.current
+              ? 'bg-gray-900 text-white'
+              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+            'block rounded-md px-3 py-2 text-base font-medium',
+          ]"
+          :aria-current="item.current ? 'page' : undefined"
+          >{{ item.name }}</DisclosureButton
+        >
       </div>
-    </div>
-  </header>
+    </DisclosurePanel>
+  </Disclosure>
 </template>
 
 <script setup>
-const isLogged = true;
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/vue";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+];
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Bungee+Tint&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap");
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 10px;
-  padding: 20px 30px;
-  border-bottom: 1px solid #c90800;
-}
-.header-logo {
-  font-family: "Bungee Tint", sans-serif !important;
-  font-weight: 500;
-  font-style: normal;
-  font-size: 30px;
-  border-right: 1px solid #c90800;
-  padding-right: 20px;
-}
-.header-categorys {
-  font-family: "Inter", sans-serif;
-  font-weight: 500;
-  font-size: 16px;
-}
-.header-right-side {
-  display: flex;
-  align-items: center;
-  gap: 20px;
-}
-.header-shopping-icon {
-  padding-right: 10px;
-  width: 35px;
-  height: 35px;
-  border-right: 1px solid #c90800;
-}
-.header-log-in {
-  cursor: pointer;
-  font-family: "Inter", sans-serif;
-  font-weight: 500;
-  font-size: 16px;
-}
-.dropdown-menu {
-  display: none;
-}
-.menu:hover .dropdown-menu {
-  display: block;
-  position: absolute;
-  border: 1px solid lightgray;
-  border-radius: 10px;
-  padding: 10px;
-  background-color: white;
-}
-.dropdown-menu ul li {
-  list-style: none;
-  margin: 10px 0;
-}
 </style>
---
+++ e-ticaret-proje/src/Fixed/oldheader.vue
@@ -0,0 +1,60 @@
+<header>
+    <div class="header-left-side flex items-center gap-8">
+      <div class="header-logo">
+        <router-link to="/">e-Market</router-link>
+      </div>
+      <div class="header-categorys flex gap-5">
+        <!-- Electronics Start -->
+        <div class="menu">
+          <RouterLink to="/Electronics">Electronics</RouterLink>
+          <div class="dropdown-menu">
+            <ul>
+              <li>
+                <router-link to="/SmartPhones">Smart Phones</router-link>
+              </li>
+              <li>
+                <router-link to="/Laptops">Laptops</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- Electronics End -->
+        <!-- Cosmetics Start -->
+        <div class="menu">
+          <RouterLink to="/Cosmetics">Cosmetics</RouterLink>
+          <div class="dropdown-menu">
+            <ul>
+              <li>
+                <router-link to="/Fragrances">Fragrances</router-link>
+              </li>
+              <li>
+                <router-link to="/Beauty">Beauty</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <!-- Cosmetics End -->
+        <RouterLink to="/Groceries">Groceries</RouterLink>
+        <RouterLink to="/Home-Decoration">Home Decoration</RouterLink>
+      </div>
+    </div>
+    <div class="header-right-side">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 576 512"
+        class="header-shopping-icon"
+      >
+        <path
+          d="M0 24C0 10.7 10.7 0 24 0L69.5 0c22 0 41.5 12.8 50.6 32l411 0c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3l-288.5 0 5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5L488 336c13.3 0 24 10.7 24 24s-10.7 24-24 24l-288.3 0c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5L24 48C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
+        />
+      </svg>
+      <div class="header-log-in">
+        <div v-if="isLogged">
+          <router-link to="Login">Giriş Yap</router-link>
+        </div>
+        <div v-else>
+          <div>Çıkış Yap</div>
+        </div>
+      </div>
+    </div>
+  </header>
